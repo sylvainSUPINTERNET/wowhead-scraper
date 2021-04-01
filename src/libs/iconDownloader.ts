28a7f0d5ae9cee2cc9f175b00839b1fe3a7adc3f
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2021-04-01 23:56:32 +0200

    [WIP]

--- src/libs/iconDownloader.ts
+++ src/libs/iconDownloader.ts
@@ -3,7 +3,6 @@
 const fs = require('fs');
 const axios = require('axios');
 
-// https://stackoverflow.com/questions/12740659/downloading-images-with-node-js
 const downloadIcon = async (picUrl: string, dirToSave: string) : Promise<any> => {
     const iconResponse = await axios.get(picUrl, {responseType: "stream"});
     iconResponse.data.pipe(fs.createWriteStream(dirToSave))
